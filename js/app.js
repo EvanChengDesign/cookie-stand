7675d24ebd923a8b206946273aa5fd66e3b424c7
Evan Cheng <evanchengdesign@gmail.com>
2024-02-26 16:04:45 -0500

    create folders and add salmon logo to img folder

--- js/app.js
+++ js/app.js
@@ -18,9 +18,9 @@ let tokyo = {
     phone: "222-222-2222",
     location: "1 Chrome-1-2 Pshiage, Sumida City, Tokyo 131-8634",
     hours: "6am, 7am, 8am, 9am, 10am, 11am, 12pm, 1pm, 2pm, 3pm, 4pm, 5pm, 6pm, 7pm",
-    minCustomers: 23,
-    maxCustomers: 65,
-    avgSale: 6.3,
+    minCustomers: 3,
+    maxCustomers: 24,
+    avgSale: 1.2,
     averageCookiesPerHour: []
 }
 
@@ -30,9 +30,9 @@ let dubai = {
     phone: "333-333-3333",
     location: "1 Sheikh Mohammed bin Rashid Blvd - Dubai",
     hours: "6am, 7am, 8am, 9am, 10am, 11am, 12pm, 1pm, 2pm, 3pm, 4pm, 5pm, 6pm, 7pm",
-    minCustomers: 23,
-    maxCustomers: 65,
-    avgSale: 6.3,
+    minCustomers: 11,
+    maxCustomers: 38,
+    avgSale: 3.7,
     averageCookiesPerHour: []
    
 }
@@ -43,9 +43,9 @@ let paris = {
     phone: "444-444-4444",
     location: "Champ de Mars, 5 Avenue Anatole France, 75007 Paris",
     hours: "6am, 7am, 8am, 9am, 10am, 11am, 12pm, 1pm, 2pm, 3pm, 4pm, 5pm, 6pm, 7pm",
-    minCustomers: 23,
-    maxCustomers: 65,
-    avgSale: 6.3,
+    minCustomers: 20,
+    maxCustomers: 38,
+    avgSale: 2.3,
     averageCookiesPerHour: []
    
 }
@@ -56,12 +56,26 @@ let lima = {
     phone: "555-555-5555",
     location: "Ca. Gral. Borgoño cuadra 8, Miraflores 15074",
     hours: "6am, 7am, 8am, 9am, 10am, 11am, 12pm, 1pm, 2pm, 3pm, 4pm, 5pm, 6pm, 7pm",
-    minCustomers: 23,
-    maxCustomers: 65,
-    avgSale: 6.3,
+    minCustomers: 2,
+    maxCustomers: 16,
+    avgSale: 4.6,
     averageCookiesPerHour: []
 }
 
+function simulateCookiesPurchased(); 
+function displayCookiesPerHour();
+function displayCityInfo();
 
-// Display results for each location
-//display
+
+
+
+
+// display results for each location
+// displayCookiesPerHour(seattle)
+// displayCookiesPerHour(tokyo)
+// displayCookiesPerHour(dubai)
+// displayCookiesPerHour(paris)
+// displayCookiesPerHour(lima)
+
+
+
