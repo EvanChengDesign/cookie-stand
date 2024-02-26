25052ffc3d8e3ea510d7b31b4498f52bc6182294
Evan Cheng <evanchengdesign@gmail.com>
2024-02-26 15:46:35 -0500

    add info for each object literal

--- app.js
+++ app.js
@@ -3,7 +3,65 @@ let hours = []
 //object literals for shop locations: 
 
 let seattle = {
-
-    name: "seattle"
-    
+    name: "Seattle",
+    phone: "123-456-7890",
+    location: "2901 3rd Ave #300, Seattle, WA 9821",
+    hours: "6am, 7am, 8am, 9am, 10am, 11am, 12pm, 1pm, 2pm, 3pm, 4pm, 5pm, 6pm, 7pm",
+    minCustomers: 23,
+    maxCustomers: 65,
+    avgSale: 6.3,
+    averageCookiesPerHour: []
 }
+
+let tokyo = {
+    name: "Tokyo",
+    phone: "222-222-2222",
+    location: "1 Chrome-1-2 Pshiage, Sumida City, Tokyo 131-8634",
+    hours: "6am, 7am, 8am, 9am, 10am, 11am, 12pm, 1pm, 2pm, 3pm, 4pm, 5pm, 6pm, 7pm",
+    minCustomers: 23,
+    maxCustomers: 65,
+    avgSale: 6.3,
+    averageCookiesPerHour: []
+}
+
+let dubai = {
+
+    name: "Dubai", 
+    phone: "333-333-3333",
+    location: "1 Sheikh Mohammed bin Rashid Blvd - Dubai",
+    hours: "6am, 7am, 8am, 9am, 10am, 11am, 12pm, 1pm, 2pm, 3pm, 4pm, 5pm, 6pm, 7pm",
+    minCustomers: 23,
+    maxCustomers: 65,
+    avgSale: 6.3,
+    averageCookiesPerHour: []
+   
+}
+
+let paris = {
+
+    name: "Paris", 
+    phone: "444-444-4444",
+    location: "Champ de Mars, 5 Avenue Anatole France, 75007 Paris",
+    hours: "6am, 7am, 8am, 9am, 10am, 11am, 12pm, 1pm, 2pm, 3pm, 4pm, 5pm, 6pm, 7pm",
+    minCustomers: 23,
+    maxCustomers: 65,
+    avgSale: 6.3,
+    averageCookiesPerHour: []
+   
+}
+
+let lima = {
+
+    name: "Lima", 
+    phone: "555-555-5555",
+    location: "Ca. Gral. Borgoño cuadra 8, Miraflores 15074",
+    hours: "6am, 7am, 8am, 9am, 10am, 11am, 12pm, 1pm, 2pm, 3pm, 4pm, 5pm, 6pm, 7pm",
+    minCustomers: 23,
+    maxCustomers: 65,
+    avgSale: 6.3,
+    averageCookiesPerHour: []
+}
+
+
+// Display results for each location
+//display
